1bbbd52cf14b8c98719d1029fae8f078817f0d4f
steven-klavins <stevenklavins94@gmail.com>
2025-10-03 18:40:43 +0100

    Generated, mapped and output recipes

--- app/routes/recipes.js
+++ app/routes/recipes.js
@@ -1,3 +1,23 @@
 import Route from '@ember/routing/route';
+import {inject as service} from '@ember/service';
 
-export default class RecipesRoute extends Route {}
+export default class RecipesRoute extends Route {
+
+    @service store;
+
+    async model() {
+        let response = await fetch('/api/recipes.json');
+        let data = await response.json();
+
+        return data.recipes.map(recipe => {
+            return this.store.createRecord('recipe', 
+                {
+                    id: recipe.id,
+                    title: recipe.title,
+                    description: recipe.description,
+                    ingredients: recipe.ingredients,
+                    instructions: recipe.instructions
+            })
+        })
+    }
+}
