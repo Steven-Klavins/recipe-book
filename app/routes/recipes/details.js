02c076000cd4a9a6eacbce90abda366bff81ff3f
steven-klavins <stevenklavins94@gmail.com>
2025-10-06 12:59:05 +0100

    Load recipe record from store on details page

--- app/routes/recipes/details.js
+++ app/routes/recipes/details.js
@@ -1,7 +1,11 @@
 import Route from '@ember/routing/route';
+import { inject as service } from "@ember/service";
 
 export default class RecipesDetailsRoute extends Route {
+
+    @service store 
+
     async model(params){
-        return true;
+        return this.store.peekRecord('recipe', params.recipe_id)
     }
 }
