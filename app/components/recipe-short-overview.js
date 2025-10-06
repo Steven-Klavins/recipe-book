afddc24def458a5387d79f9e96531994cc26f611
steven-klavins <stevenklavins94@gmail.com>
2025-10-06 12:20:53 +0100

    Add toggle functionality to recipes short component

--- app/components/recipe-short-overview.js
+++ app/components/recipe-short-overview.js
@@ -1,3 +1,12 @@
-import Component from '@glimmer/component';
+import Component from "@glimmer/component";
+import { action } from "@ember/object";
+import { tracked } from "@glimmer/tracking";
 
-export default class RecipeShortOverview extends Component {}
+export default class RecipeShortOverview extends Component {
+  @tracked isExpanded = false;
+
+  @action
+  toggleExpand() {
+    this.isExpanded = !this.isExpanded;
+  }
+}
