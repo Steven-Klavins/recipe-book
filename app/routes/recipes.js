68d877c60e5cea03e7fe1e8ad6888cb6009ac233
steven-klavins <stevenklavins94@gmail.com>
2025-10-06 12:45:19 +0100

    Ensure recipes are only loaded into store once

--- app/routes/recipes.js
+++ app/routes/recipes.js
@@ -1,23 +1,30 @@
-import Route from '@ember/routing/route';
-import {inject as service} from '@ember/service';
+import Route from "@ember/routing/route";
+import { inject as service } from "@ember/service";
 
 export default class RecipesRoute extends Route {
+  @service store;
 
-    @service store;
+  async model() {
+    let response = await fetch("/api/recipes.json");
+    let data = await response.json();
 
-    async model() {
-        let response = await fetch('/api/recipes.json');
-        let data = await response.json();
+    return data.recipes.map((recipe) => {
 
-        return data.recipes.map(recipe => {
-            return this.store.createRecord('recipe', 
-                {
-                    id: recipe.id,
-                    title: recipe.title,
-                    description: recipe.description,
-                    ingredients: recipe.ingredients,
-                    instructions: recipe.instructions
-            })
-        })
-    }
+      // Check to see if the record is already loaded into our current store
+      let existingRecipe = this.store.peekRecord("recipe", recipe.id);
+
+      if (!existingRecipe) {
+        return this.store.createRecord("recipe", {
+          id: recipe.id,
+          title: recipe.title,
+          description: recipe.description,
+          ingredients: recipe.ingredients,
+          instructions: recipe.instructions,
+        });
+      }
+      else {
+        return existingRecipe
+      }
+    });
+  }
 }
