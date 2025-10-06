94727752c2418e05f2b5b2c4f7b14bdecd298cf2
steven-klavins <stevenklavins94@gmail.com>
2025-10-06 21:02:22 +0100

    Display recipes stored locally and generate id's for new records

--- app/services/recipe-data.js
+++ app/services/recipe-data.js
@@ -1,14 +1,15 @@
 import Service from "@ember/service";
 import { inject as service } from "@ember/service";
 export default class RecipeDataService extends Service {
-
   @service store;
 
   async loadRecipes() {
     let response = await fetch("/api/recipes.json");
     let data = await response.json();
+    // Temp local storage implimentation
+    let storedRecipes = JSON.parse(localStorage.getItem("recipes")) || [];
 
-    return data.recipes.map((recipe) => {
+    const addRecipeToStore = (recipe) => {
       // Check to see if the record is already loaded into our current store
       let existingRecipe = this.store.peekRecord("recipe", recipe.id);
 
@@ -23,13 +24,24 @@ export default class RecipeDataService extends Service {
       } else {
         return existingRecipe;
       }
-    });
+    };
+
+    storedRecipes.forEach(addRecipeToStore);
+    return data.recipes.map(addRecipeToStore);
   }
 
   // Save to local storage for the sake of testing the app independent of a backend.
   async saveRecipe(recipe) {
-    let storedRecipes = JSON.parse(localStorage.getItem('recipes')) || []
-    storedRecipes.push(recipe)
-    localStorage.setItem('recipes', JSON.stringify(storedRecipes))
+    let storedRecipes = JSON.parse(localStorage.getItem("recipes")) || [];
+    storedRecipes.push(recipe);
+    localStorage.setItem("recipes", JSON.stringify(storedRecipes));
+  }
+
+  generateGUID() {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+      var r = (Math.random() * 16) | 0,
+        v = c === "x" ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    });
   }
 }
